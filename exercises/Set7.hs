f8cf591c1b382d702b08cf66e8aa34521c6e2995
Akira Taguchi <akirahattivatti@gmail.com>
2022-10-14 09:15:39 +0300

    Complete 7.2

--- exercises/Set7.hs
+++ exercises/Set7.hs
@@ -49,15 +49,17 @@ data Set a = Set [a]
 
 -- emptySet is a set with no elements
 emptySet :: Set a
-emptySet = todo
+emptySet = Set []
 
 -- member tests if an element is in a set
 member :: Eq a => a -> Set a -> Bool
-member = todo
+member a (Set xs) = a `elem` xs
 
 -- add a member to a set
-add :: a -> Set a -> Set a
-add = todo
+add :: (Ord a, Eq a) => a -> Set a -> Set a
+add a (Set xs)
+  | not (member a (Set xs)) = Set $ sort $ (++) xs [a]
+  | otherwise = Set xs
 
 ------------------------------------------------------------------------------
 -- Ex 3: a state machine for baking a cake. The type Event represents
